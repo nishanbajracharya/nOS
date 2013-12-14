3dcf97ab1bcde8bf14983f647284b1a1f7757829
Nishan Bajracharya <nishan_cd@ymail.com>
2013-12-14 18:14:17 +0545

    Apps Board

--- system/scripts/desktop.js
+++ system/scripts/desktop.js
@@ -5,14 +5,26 @@ $(function() {
 	$(".icon-draggable li").draggable();
 	$("#apps").click(function(){
 		$("#app-menu").fadeIn();
+		$(".icon-draggable").addClass("moveDownClass",'normal')
+		toggled=true;
 	})
 	$("#documents").dblclick(function(){
 
 	})
+	toggled = false;
 	$(document).keyup(function(e) {
 		if (e.keyCode == 18) {
 			e.preventDefault();
 			$("#app-menu").fadeToggle();
+			if(!toggled){
+				$(".icon-draggable").removeClass("moveUpClass")
+				$(".icon-draggable").addClass("moveDownClass",'fast')
+				toggled=true;
+			}else{
+				$(".icon-draggable").addClass("moveUpClass",'normal')
+				$(".icon-draggable").delay(500).removeClass("moveDownClass")
+				toggled=false;
+			}
 		}   
 	});
 });
